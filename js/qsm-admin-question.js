c1b819adbf7c0bba9a748cd2ec07a3177d8cded7
Frank Corso <frank@mylocalwebstop.com>
2017-08-10 18:38:12 -0400

    Begins adding editing question view

--- js/qsm-admin-question.js
+++ js/qsm-admin-question.js
@@ -5,15 +5,26 @@
 var QSMQuestion;
 (function ($) {
 	QSMQuestion = {
-		createNewPage: function() {
+		addNewPage: function() {
 			var template = _.template( jQuery( '#page-tmpl' ).html() );
-			$( 'questions' ).append( template() );
+			$( '.questions' ).append( template() );
+			$( '.page' ).sortable({
+				opacity: 70,
+				cursor: 'move',
+				placeholder: "ui-state-highlight",
+				connectWith: '.page'
+			});
 			setTimeout( QSMQuestion.removeNew, 250 );
 		},
 		createNewQuestion: function( page ) {
 			var template = _.template( jQuery( '#question-tmpl' ).html() );
 			page.append( template( { type : 'Large Open Answer', category : 'Math', question: 'Some random question' } ) );
 			setTimeout( QSMQuestion.removeNew, 250 );
+		},
+		editQuestion: function( $question ) {
+			var template = _.template( jQuery( '#edit-question-tmpl' ).html() );
+			$question.append( template() )
+
 		},
 		removeNew: function() {
 			$( '.page-new' ).removeClass( 'page-new' );
@@ -24,12 +35,17 @@ var QSMQuestion;
 	$(function() {
 		$( '.new-page-button' ).on( 'click', function( event ) {
 			event.preventDefault();
-			QSMQuestion.createNewPage();
+			QSMQuestion.addNewPage();
 		});
 
 		$( '.questions' ).on( 'click', '.new-question-button', function( event ) {
 			event.preventDefault();
 			QSMQuestion.createNewQuestion( $( this ).parent() );
+		});
+
+			$( '.questions' ).on( 'click', '.question', function( event ) {
+			event.preventDefault();
+			QSMQuestion.editQuestion( $( this ) );
 		});
 
 		$( '.questions' ).sortable({
